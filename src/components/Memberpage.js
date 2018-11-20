b3c19be8462f34fea276b5f65e8a1529dbbd5e81
jjung <kimje487@gmail.com>
2018-11-21 01:15:45 +0900

    add jjung info

--- src/components/Memberpage.js
+++ src/components/Memberpage.js
@@ -2,9 +2,10 @@ import React from "react";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import "./Memberpage.css"
-import ilseopimg from '../img/ilseop.jpg'
-import geonhoimg from '../img/geonho.jpg'
+import "./Memberpage.css";
+import ilseopimg from "../img/ilseop.jpg";
+import geonhoimg from "../img/geonho.jpg";
+import jjungimg from "../img/jjung.jpg";
 
 const Memberpage = () => {
   return (
@@ -13,100 +14,50 @@ const Memberpage = () => {
         <a href="https://master.cerebroscholar.com">Back</a>
       </div>
 
-      <h2 className="h1-responsive font-weight-bold my-5">
-        Members Introduction
-      </h2>
+      <h2 className="h1-responsive font-weight-bold my-5">Meet the team!</h2>
       <p className="grey-text w-responsive mx-auto mb-5">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error
-        amet numquam iure provident voluptate esse quasi, veritatis totam
-        voluptas nostrum quisquam eum porro a pariatur veniam.
+        We are interested in building efficient research ecosystem with data.
       </p>
 
       <div className="row">
         <div className="col-lg-4 col-md-6 mb-lg-0 mb-5">
           <div className="avatar mx-auto">
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg"
-              className="rounded-circle z-depth-1"
+              src={geonhoimg}
+              className="rounded-circle z-depth-1 ilseop"
               alt="Sample avatar"
             />
           </div>
-          <h5 className="font-weight-bold mt-4 mb-3">John Doe</h5>
+          <h5 className="font-weight-bold mt-4 mb-3">GeonHo Han</h5>
           <p className="text-uppercase blue-text">
-            <strong>Web developer</strong>
+            <strong>Web & Dev Ops & AWS</strong>
           </p>
           <p className="grey-text">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem ipsa
-            accusantium doloremque rem laudantium totam aperiam.
+            Code Base management, Integrations, CI/CD System in this project.{" "}
+            <br /> Interests are in Data.
+            <br />{" "}
           </p>
           <ul className="list-unstyled mb-0">
-            <a className="p-2 fa-lg fb-ic">
-              <i className="fa fa-facebook blue-text"> </i>
+            <a className="p-2 fa-lg fb-ic" href="https://github.com/hrg921">
+              <i className="fa fa-github blue-text"> </i>
             </a>
-            <a className="p-2 fa-lg ins-ic">
-              <i className="fa fa-instagram blue-text"> </i>
+            <a className="p-2 fa-lg mail-ic" href="mailto:hrg921@gmail.com">
+              <i className="fa fa-envelope blue-text"> </i>
+            </a>
+            <a
+              className="p-2 fa-lg resume-ic"
+              href="https://hrg921.github.io/resume/"
+            >
+              <i className="fa fa-file blue-text"> </i>
             </a>
           </ul>
         </div>
 
-		    <div className="col-lg-4 col-md-6 mb-lg-0 mb-5">
-		      <div className="avatar mx-auto">
-		        <img src={geonhoimg} className="rounded-circle z-depth-1 ilseop"
-		          alt="Sample avatar" />
-		      </div>
-		      <h5 className="font-weight-bold mt-4 mb-3">GeonHo Han</h5>
-		      <p className="text-uppercase blue-text"><strong>Web & Dev Ops & AWS</strong></p>
-		      <p className="grey-text">Code Base management, Integrations, CI/CD System in this project. <br/> Interests are in Data.
-						<br/>  </p>
-		      <ul className="list-unstyled mb-0">
-		                <a className="p-2 fa-lg fb-ic" href="https://github.com/hrg921">
-		          <i className="fa fa-github blue-text"> </i>
-		        </a>
-		                <a className="p-2 fa-lg mail-ic" href="mailto:hrg921@gmail.com">
-		          <i className="fa fa-envelope blue-text"> </i>
-		        </a>
-						<a className="p-2 fa-lg resume-ic" href="https://hrg921.github.io/resume/">
-							<i className="fa fa-file blue-text"> </i>
-						</a>
-		      </ul>
-		    </div>
-		    
-		    <div className="col-lg-4 col-md-6 mb-md-0 mb-5">
-		      <div className="avatar mx-auto">
-		        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" className="rounded-circle z-depth-1"
-		          alt="Sample avatar" />
-		      </div>
-		      <h5 className="font-weight-bold mt-4 mb-3">Maria Smith</h5>
-		      <p className="text-uppercase blue-text"><strong>Photographer</strong></p>
-		      <p className="grey-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-		        mollit anim est fugiat nulla id eu laborum.</p>
-		      <ul className="list-unstyled mb-0">
-		                <a className="p-2 fa-lg fb-ic">
-		          <i className="fa fa-facebook blue-text"> </i>
-		        </a>
-		                <a className="p-2 fa-lg ins-ic">
-		          <i className="fa fa-instagram blue-text"> </i>
-		        </a>
-		                <a className="p-2 fa-lg ins-ic">
-		          <i className="fa fa-dribbble blue-text"> </i>
-		        </a>
-		      </ul>
-		    </div>
-		    
-		    <div className="col-lg-4 col-md-6">
-		      <div className="avatar mx-auto">
-		        <img src={ilseopimg} className="rounded-circle z-depth-1 ilseop" alt="ilseop" />
-		      </div>
-		      <h5 className="font-weight-bold mt-4 mb-3">IL SEOP LEE</h5>
-		      <p className="text-uppercase blue-text"><strong>Data Engineering & Backend API</strong></p>
-		      <p className="grey-text">I'm interested in Data Engineering, Information Retrieval, Natural Language Processing.</p>
-		      <ul className="list-unstyled mb-0">
-		        {/*<a className="p-2 fa-lg fb-ic">
-        <div className="col-lg-4 col-md-6 mb-md-0 mb-5">
+        <div className="col-lg-4 col-md-6">
           <div className="avatar mx-auto">
             <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
-              className="rounded-circle z-depth-1"
+              src={jjungimg}
+              className="rounded-circle z-depth-1 ilseop"
               alt="Sample avatar"
             />
           </div>
@@ -115,10 +66,11 @@ const Memberpage = () => {
             <strong>Team Leader. Data Analysis & Visualization</strong>
           </p>
           <p className="grey-text">
-            I'm interesed in making a world a better place by solving an
-            everyday problem. <br />
-            This project will be go on to build efficient research ecosystem. If
-            Cerebro helped you please email below!:)
+            I love to solve an everyday problem and building solutions. <br />
+            Skillset : Deep Learning, Find Insights from data.
+            <br />
+            Cerebro will be go on, email below to contact continuous-project.
+            <br />
           </p>
           <ul className="list-unstyled mb-0">
             {/* <a className="p-2 fa-lg fb-ic">
